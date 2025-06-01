70201ebc9e8fdc3d1d7e7fdbfedd1a7aadf0e467
rikzaafnan <rikzaafnan@gmail.com>
2025-06-01 23:17:48 +0700

    add page register

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Layout from './components/Layout'
+import UserRegister from './components/User/UserRegister'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout/>} >
+          <Route path="/register" element={<UserRegister/>}>
+
+          </Route>
+        </Route>
+        <Route path="/dashboard" element={<div>dashboard</div>}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
